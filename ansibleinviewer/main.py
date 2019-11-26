74c07467219a5cb90bd7b18e3d2accc8f45dd12a
Piotr Sykulski <pitersk@gmail.com>
2019-11-26 19:27:37 +0100

    Modify travis config. Add setting the tiled tmux layout after each window split, to avoid splitting only one window, which would fail after about 8 hosts, as the pane would have the minimal size.

--- ansibleinviewer/main.py
+++ ansibleinviewer/main.py
@@ -32,8 +32,8 @@ def create_tmux_script(hosts: List[AnsibleHostAdapter]) -> str:
         tmux_file_lines.append("send-keys '{}' C-m".format(host.connection_command))
         if index != len(hosts) - 1:
             tmux_file_lines.append("split-window -h")
-    # tiled layout rearranges panes so that each pane has the same size
-    tmux_file_lines.append("select-layout tiled")
+            # tiled layout rearranges panes so that each pane has the same size
+            tmux_file_lines.append("select-layout tiled")
     # \; is printed so that ; can be interpreted by tmux instead of shell
     tmux_script = " \\; ".join(tmux_file_lines)
     return tmux_script
