a857175df09ea95224c23375531e43ec881d2d35
Piotr Sykulski <pitersk@gmail.com>
2019-12-29 16:48:30 +0100

    Prepare to publish the package on PyPI

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-from setuptools import setup, find_packages
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
 
 
 def requirements(filename):
@@ -9,11 +14,26 @@ def requirements(filename):
 
 setup(
     name='ansibleconnect',
-    version='0.1',
+    version='0.1.0',
+    description='Connect to all hosts from the ansible inventory with one command',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/psykulsk/ansibleconnect",
     author='Piotr Sykulski',
     author_email='pitersk@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Topic :: Utilities',
+        'Topic :: System :: Systems Administration'
+    ],
     install_requires=requirements('requirements.txt'),
-    packages=find_packages(),
+    packages=['ansibleconnect'],
     entry_points='''
     [console_scripts]
     ansibleconnect=ansibleconnect.main:main
